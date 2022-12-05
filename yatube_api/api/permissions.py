6b58bcd382057fca30a98247fe7525f0f5596108
Сергей Мартынов <40.10.444@gmail.com>
2022-12-05 18:13:51 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	yatube_api/posts/models.py

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 
 class AuthorPermission(permissions.BasePermission):
-    def has_objects_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user)
