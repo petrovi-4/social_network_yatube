935b5f35b8eec9afbd8ddd067a55d145ac0183f8
Сергей Мартынов <40.10.444@gmail.com>
2022-12-05 14:27:22 +0300

    Version for review 1.3

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -19,6 +19,13 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts'
     )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='posts',
+    )
     image = models.ImageField(upload_to='posts/', null=True, blank=True)
 
     def __str__(self):
