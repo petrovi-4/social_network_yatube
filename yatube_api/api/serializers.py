da388503974a96b5527968aaa00f7fcc61f5c1e2
Сергей Мартынов <40.10.444@gmail.com>
2022-12-05 14:09:04 +0300

    Version for review 1.2

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -6,53 +6,53 @@ from posts.models import Comment, Post, Follow, User, Group
 
 
 class PostSerializer(serializers.ModelSerializer):
-	author = SlugRelatedField(slug_field='username', read_only=True)
+    author = SlugRelatedField(slug_field='username', read_only=True)
 
-	class Meta:
-		fields = '__all__'
-		model = Post
+    class Meta:
+        fields = '__all__'
+        model = Post
 
 
 class CommentSerializer(serializers.ModelSerializer):
-	author = serializers.SlugRelatedField(
-		read_only=True, slug_field='username'
-	)
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
 
-	class Meta:
-		fields = '__all__'
-		model = Comment
+    class Meta:
+        fields = '__all__'
+        model = Comment
 
 
 class GroupSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = 'Group'
-		fields = '__all__'
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 
 class FollowSerializer(serializers.ModelSerializer):
-	user = serializers.SlugRelatedField(
-		slug_field='username',
-		queryset=User.objects.all(),
-		default=serializers.CurrentUserDefault(),
-	)
-	following = serializers.SlugRelatedField(
-		slug_field='username', queryset=User.objects.all()
-	)
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault(),
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username', queryset=User.objects.all()
+    )
 
-	class Meta:
-		model = Follow
-		fields = '__all__'
-		validators = (
-			UniqueTogetherValidator(
-				queryset=Follow.objects.all(),
-				fields=('user', 'following'),
-				message=('Подписка на автора уже оформлена!'),
-			),
-		)
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        validators = (
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message=('Подписка на автора уже оформлена!')
+            ),
+        )
 
-	def validate(self, data):
-		if data['user'] == data['following']:
-			raise serializers.ValidationError(
-				'Нельзя отписаться от самого себя!'
-			)
-		return data
+    def validate(self, data):
+        if data['user'] == data['following']:
+            raise serializers.ValidationError(
+                'Нельзя отписаться от самого себя!'
+            )
+        return data
